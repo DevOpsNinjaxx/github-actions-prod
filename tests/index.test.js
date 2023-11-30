2c6e056f7fcbd4b2c14d42c2d672f4967a654412
Aminu Ndakun <Amin4712@gmail.com>
2023-11-30 18:44:22 +0100

    added new feature

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,9 +4,9 @@ const request = require('supertest');
 const app = require('../index'); // Assuming your Express app logic is in index.js
 
 describe('GET /', () => {
-  it('responds with "Hello World! FROM GITHUB ACTION!!"', async () => {
+  it('responds with "Hello World! FROM GITHUB ACTION!! Making a minor change"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello World! FROM GITHUB ACTION!!');
+    expect(response.text).toBe('Hello World! FROM GITHUB ACTION!! Making a minor change');
   });
 });
