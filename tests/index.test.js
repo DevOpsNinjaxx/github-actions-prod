785f7d05dc0b8ab96dc6a7570a773f391ef6f751
DevOpsNinjaxx <137789137+DevOpsNinjaxx@users.noreply.github.com>
2023-11-30 21:13:48 +0100

    Newfeature (#23)
    * adding codeQL scanning
    
    * added new feature
    
    * added new feature
    
    * added new feature
    
    * added new feature
    
    * added new feature
    
    * added new feature
    
    * added new feature
    
    ---------
    
    Co-authored-by: Aminu Ndakun <Amin4712@gmail.com>

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,8 +4,9 @@ const request = require('supertest');
 const app = require('../index'); // Assuming your Express app logic is in index.js
 
 describe('GET /', () => {
-  it('responds with "Hello World! making a small test"', async () => {
+  it('responds with "Hello World! FROM GITHUB ACTION!! Making a minor change"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello World! FROM GITHUB ACTION!! Making a minor change');
   });
 });
