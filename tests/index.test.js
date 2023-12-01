632fb13d9cd895fe68c73eb30a08ca387eaebeb4
Aminu Ndakun <Amin4712@gmail.com>
2023-12-01 17:01:44 +0100

    PR 26 TESTING

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,9 +4,9 @@ const request = require('supertest');
 const app = require('../index'); // Assuming your Express app logic is in index.js
 
 describe('GET /', () => {
-  it('responds with "PR TEST 1"', async () => {
+  it('responds with "PR TEST 26"', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('PR TEST 1');
+    expect(response.text).toBe('PR TEST 26');
   });
 });
